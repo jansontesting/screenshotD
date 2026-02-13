0339e4488be5e9338f13ef501a4810a74210ca21
jansontesting <jansontestingsoftwares@gmail.com>
2026-02-13 20:46:20 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@sparticuz/chromium-min'],
+  serverExternalPackages: ['@sparticuz/chromium'],
 };
 
 export default nextConfig;
